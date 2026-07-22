d01c93c92ee1b73cb71dbefb960879077aaec202
Muhammad Zain Haseeb <zainhaseeb474@gmail.com>
2026-07-22 16:32:32 +0500

    commented inside of app.py

--- server/app.py
+++ server/app.py
@@ -27,6 +27,7 @@ ALLOWED_CONTENT_TYPES = {
     "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
 }
 
+# added and removed rstrip()
 frontend_url = os.getenv("FRONTEND_URL", "http://localhost:5173").rstrip("/")
 
 app = FastAPI(title="DocuChat API", version="1.0.0")
